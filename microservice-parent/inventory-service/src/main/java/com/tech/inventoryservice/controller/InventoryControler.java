e58b94e6543e0bde4a6ac39b89b48db03de66b9a
pawan.yadav <pawan.yadav@algosec.com>
2023-02-26 20:16:59 +0530

    Add hardcoded communication between order service and the inventory service

--- microservice-parent/inventory-service/src/main/java/com/tech/inventoryservice/controller/InventoryControler.java
+++ microservice-parent/inventory-service/src/main/java/com/tech/inventoryservice/controller/InventoryControler.java
@@ -1,13 +1,17 @@
 package com.tech.inventoryservice.controller;
 
+import com.tech.inventoryservice.dto.InventoryResponse;
 import com.tech.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -15,9 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class InventoryControler {
 
     private final InventoryService inventoryService;
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
        return inventoryService.isInStock(skuCode);
     }
 
--- microservice-parent/order-service/src/main/java/com/tech/orderservice/service/OrderService.java
+++ microservice-parent/order-service/src/main/java/com/tech/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.tech.orderservice.service;
 
+import com.tech.orderservice.dto.InventoryResponse;
 import com.tech.orderservice.dto.OrderLineItemsDto;
 import com.tech.orderservice.dto.OrderRequest;
 import com.tech.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.tech.orderservice.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,6 +20,8 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
+
+    private final WebClient webClient;
     public void placeOder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -25,7 +30,24 @@ public class OrderService {
             .map(this::mapToDto).toList();
 
         order.setOrderLineItemsList(orderLineItems);
-        orderRepository.save(order);
+
+        final List<String> skus = orderLineItems.stream().map(OrderLineItems::getSkuCode).toList();
+
+        // Check if inventory avl.
+        final InventoryResponse[] inventoryResponses = webClient.get()
+            .uri("http://localhost:8082/api/inventory",
+                uriBuilder -> uriBuilder.queryParam("skuCode", skus).build())
+            .retrieve()
+            .bodyToMono(InventoryResponse[].class)
+            .block();
+
+        final boolean allProductsInStock = Arrays.stream(inventoryResponses != null ? inventoryResponses : new InventoryResponse[0]).allMatch(InventoryResponse::isInStock);
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, Please try again later.");
+        }
+
     }
 
     private OrderLineItems mapToDto(final OrderLineItemsDto order) {
